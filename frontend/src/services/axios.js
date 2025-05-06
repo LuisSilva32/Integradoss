c5371edf35d0866c995dc5b0893409a92ba36c4a
Black246 <martineliasdeleonpertuz@gmail.com>
2025-05-06 14:14:17 -0500

    configuración url

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 // Crear una instancia de axios
 const axiosInstance = axios.create({
-  baseURL: 'https://api.integradoss.com/api'
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 // Interceptor para manejar errores
